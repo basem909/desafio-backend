f101a2fec96006032006eaa135da481442bad448
basem909 <basemshams30@gmail.com>
2023-01-12 03:28:21 -0300

    Create a find_last_complete_campaign method

--- app/controllers/desafio_controller.rb
+++ app/controllers/desafio_controller.rb
@@ -1,2 +1,9 @@
 class DesafioController < ApplicationController
+
+  def find_last_completed_campaign
+    @last_completed = CampaignState.where(state: :completed).last
+    @campaign = Campaign.where(id: @last_completed.campaign_id)
+
+    return @campaign
+  end
 end
