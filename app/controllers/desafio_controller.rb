5feb063331c31f63950e85e2bdd45f2f86d06299
basem909 <basemshams30@gmail.com>
2023-01-12 03:32:04 -0300

    Add illustrative comments

--- app/controllers/desafio_controller.rb
+++ app/controllers/desafio_controller.rb
@@ -1,9 +1,14 @@
 class DesafioController < ApplicationController
 
   def find_last_completed_campaign
+    # Get the last completed state on Campaign_State table
     @last_completed = CampaignState.where(state: :completed).last
-    @campaign = Campaign.where(id: @last_completed.campaign_id)
+
+    # Get the campaign object that turned to completed most recently
+    @campaign = Campaign.where(id: @last_completed.campaign_id).first
 
     return @campaign
   end
+
+  
 end
