210e31e1e6450649d9ef3a610cd5d45d6b341928
basem909 <basemshams30@gmail.com>
2023-01-12 17:43:58 -0300

    Finish first draft of fucntioning code for the 3 challenges

--- app/controllers/desafio_controller.rb
+++ app/controllers/desafio_controller.rb
@@ -3,22 +3,25 @@ class DesafioController < ApplicationController
   def find_last_completed_campaign
     # Get the last completed state on Campaign_State table
     @last_completed = Campaign.joins(:campaign_states).where(campaign_states: {state: :completed}).last
-
-    return @last_completed
+    
+    @last_completed
   end
 
   def list_campaigns_by_state(state)
     @current_list = []
-    @list = Campaign.joins(:campaign_states).where(campaign_states: {state: state}).group(:campaign_states)
+    @list = Campaign.joins(:campaign_states).where(campaign_states: {state: state})
     @list.each do |camp|
-      @current_list << camp if camp.campaign_states.last.state == state.to_s
+      @current_list << camp.name if camp.campaign_states.last.state == state.to_s
     end
     @current_list
   end
 
   def total_campaigns_by_state
-    @campaigns = CampaignState.group(:state).count
-    @total = 
-
+    @total = []
+    @states = [ :draft, :scheduled, :processing, :sending, :completed, :error ]
+    @states.each do |state|
+      @total << {state: state, campaigns: list_campaigns_by_state(state).length}
+    end
+    @total
   end
 end
