ea43de0dfc6d5c31897600bbf5e7f739093599f9
basem909 <basemshams30@gmail.com>
2023-01-13 16:38:25 -0300

    Fix linter errors

--- spec/requests/campaigns_request_spec.rb
+++ spec/requests/campaigns_request_spec.rb
@@ -25,38 +25,38 @@ RSpec.describe Campaign, type: :request do
       post '/campaigns/list_state?state=draft'
 
       expect(response).to have_http_status(200)
-      expect(response.body).to include("Campaign A","Campaign AB","Campaign AC")
+      expect(response.body).to include('Campaign A', 'Campaign AB', 'Campaign AC')
     end
 
     it 'is receiving an Ok response from the server and return campaign B' do
       post '/campaigns/list_state?state=scheduled'
 
       expect(response).to have_http_status(200)
-      expect(response.body).to include("Campaign B")
+      expect(response.body).to include('Campaign B')
     end
 
     it 'is receiving an Ok response from the server and return campaign C and CA' do
       post '/campaigns/list_state?state=processing'
 
       expect(response).to have_http_status(200)
-      expect(response.body).to include("Campaign C","Campaign CA")
+      expect(response.body).to include('Campaign C', 'Campaign CA')
     end
 
     it 'is receiving an Ok response from the server and return campaign D' do
       post '/campaigns/list_state?state=sending'
 
       expect(response).to have_http_status(200)
-      expect(response.body).to include("Campaign D")
+      expect(response.body).to include('Campaign D')
     end
 
     it 'is receiving an Ok response from the server and return campaign E and EA' do
       post '/campaigns/list_state?state=completed'
 
       expect(response).to have_http_status(200)
-      expect(response.body).to include("Campaign E","Campaign EA")
+      expect(response.body).to include('Campaign E', 'Campaign EA')
     end
   end
-  
+
   context 'use get request total_state to get the total state' do
     it 'is receiving an Ok response from the server' do
       get '/campaigns/total_state'
@@ -65,7 +65,9 @@ RSpec.describe Campaign, type: :request do
 
     it 'the response return the list of each state and the current number of campaigns' do
       get '/campaigns/total_state'
-      expect(response.body).to eq("[{\"campaigns\":3,\"state\":\"draft\"},{\"campaigns\":1,\"state\":\"scheduled\"},{\"campaigns\":2,\"state\":\"processing\"},{\"campaigns\":1,\"state\":\"sending\"},{\"campaigns\":2,\"state\":\"completed\"},{\"campaigns\":1,\"state\":\"error\"}]")
+      expect(response.body).to eq('[{"campaigns":3,"state":"draft"},{"campaigns":1,"state":"scheduled"},{"campaigns":2,
+      "state":"processing"},{"campaigns":1,"state":"sending"}
+      ,{"campaigns":2,"state":"completed"},{"campaigns":1,"state":"error"}]')
     end
   end
 end
